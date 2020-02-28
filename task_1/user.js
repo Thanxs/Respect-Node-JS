77cb6e027d1c261aae7e33c21ea5655411275b05
Oleksandr Nikolskyi <nikolskiy90@gmail.com>
2020-02-28 20:58:37 +0200

    task_1_fixed

--- task_1/user.js
+++
@@ -1,9 +0,0 @@
-const chalk = require("chalk");
-
-exports.user = {
-  name: "Alex",
-  position: "NodeJS developer",
-  sayHello() {
-    console.log(chalk.magenta(`Hello, my name is ${this.name} and I'm ${this.position}`));
-  }
-};
